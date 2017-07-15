d7335cf75ae523de0c088aeb5495edddacf0d7f9
Jenny Lai <jenny.lai@flatironschool.com>
2017-07-15 18:06:29 -0400

    refactored created a component file StorePicker

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,13 +1,8 @@
 // let's go!
 import React from 'react';
 import { render } from 'react-dom';
+import StorePicker from './components/StorePicker';
 
-
-class StorePicker extends React.Component {
-  render() {
-    return <p>Hello!</p>
-  }
-}
 
 
 render(<StorePicker/>, document.querySelector('#main'));
